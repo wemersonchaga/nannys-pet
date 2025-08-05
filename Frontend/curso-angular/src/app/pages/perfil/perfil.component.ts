0ffcaa0a5c8e8721ee80f345964752a1450dc542
wemersonchaga <135094679+wemersonchaga@users.noreply.github.com>
2025-08-05 05:37:33 +0000

    Corrige caminho da imagem para usar

--- Frontend/curso-angular/src/app/pages/perfil/perfil.component.ts
+++ Frontend/curso-angular/src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PerfilService } from '../../services/perfil.service';
+import { CuidadorService } from '../../services/cuidador.service';
+import { Caracteristicas } from '../../Caracteristicas';
+import { CaracteristicasService } from '../../services/caracteristicas.service';
+import { Porte } from '../../Porte';
+
 
 @Component({
   selector: 'app-perfil',
@@ -11,22 +16,73 @@ export class PerfilComponent implements OnInit {
   userData: any;
   perfilTipo: 'tutor' | 'cuidador' | 'nenhum' = 'nenhum';
   pets: any[] = [];
+  caracteristicasDisponiveis: Caracteristicas[] = [];
+  portesDisponiveis: Porte[] = [];
+
 
   constructor(
     private perfilService: PerfilService,
+    private caracteristicasService: CaracteristicasService,
+    private cuidadorService:CuidadorService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.loadUserData();
+    this.loadCaracteristicas();
+    this.loadPortesDisponiveis();
   }
 
-  get portesAceitosFormatados(): string {
-    if (!this.userData || !this.userData.portes_aceitos) {
+  private loadCaracteristicas(): void {
+    this.caracteristicasService.getCaracteristicas().subscribe({
+      next: (data) => {
+        this.caracteristicasDisponiveis = data;
+      },
+      error: (err) => {
+        console.error('Erro ao carregar características disponíveis:', err);
+      }
+    });
+  }
+
+  private loadPortesDisponiveis(): void {
+  this.cuidadorService.getPortes().subscribe({
+    next: (data) => {
+      this.portesDisponiveis = data;
+    },
+    error: (err) => {
+      console.error('Erro ao carregar portes:', err);
+    }
+  });
+}
+
+  get caracteristicasCuidadorFormatadas(): string {
+    if (!this.userData?.caracteristicas || !this.caracteristicasDisponiveis?.length) {
       return '';
     }
-    return this.userData.portes_aceitos.map((p: any) => p.nome).join(', ');
+    return this.userData.caracteristicas
+    .map((id: number) => this.getNomeCaracteristicaPorId(id))
+    .join(', ');
   }
+
+
+  get portesAceitosFormatados(): string {
+    if (!this.userData?.portes_aceitos || !Array.isArray(this.userData.portes_aceitos)) return '';
+    return this.userData.portes_aceitos
+    .map((id: number) => this.getNomePortePorId(id))
+    .join(', ');
+  }
+
+
+  getNomeCaracteristicaPorId(id: number): string {
+    const caract = this.caracteristicasDisponiveis.find(c => c.id === id);
+    return caract ? caract.nome : 'Desconhecido';
+  }
+
+  getNomePortePorId(id: number): string {
+    const porte = this.portesDisponiveis.find(p => p.id === id);
+    return porte ? porte.nome : 'Desconhecido';
+  }
+
 
   loadUserData(): void {
     this.perfilService.getPerfilTutor().subscribe({
